84944ee43c0d2f9ad893a9bd0b4d22b725add4f3
Akraja712 <ashokkumar7hitter@gmail.com>
2024-04-26 12:29:12 +0530

    header.php

--- public/login-form.php
+++ public/login-form.php
@@ -57,7 +57,7 @@ if (isset($_POST['btnLogin'])) {
     <!-- general form elements -->
     <div class='row'>
         <div class="col-md-12 text-center">
-            <img src="dist/img/data.jpeg" height="110">
+            <img src="dist/img/ratings.jpeg" height="110">
             <h3>Ratings Job-Dashboard</h3>
         </div>
         <div class="box box-info col-md-12">
